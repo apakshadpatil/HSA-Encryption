41106e93b6965acbbc74fa3c1d87bec4223e0237
hitesh1305 <142805472+hitesh1305@users.noreply.github.com>
2024-04-19 15:32:32 +0530

    Create hex to char .cpp

---
+++ hex to char .cpp
@@ -0,0 +1,5 @@
+                stringstream ss;
+                ss<<hex<<a[k][i][j];
+                unsigned int hexv;
+                ss>>hexv;
+                cout<<"["<<static_cast<char>(hexv)<<"] ";
